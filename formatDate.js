e92aac0b98634e11c2b7521ea4beb53339d2e616
Zhigalov <sergey.zhigalov@gmail.com>
2017-04-26 14:09:47 +0500

    Формулировка задачи

---
+++ formatDate.js
@@ -0,0 +1,5 @@
+function formatDate(date) {
+    // Напишите код форматирования даты в этом месте
+}
+
+module.exports = formatDate;
---
+++ showTweets.js
@@ -0,0 +1,8 @@
+const formatDate = require('./formatDate');
+
+function showTweets() {
+    // Здесь будет код, который получает твиты и
+    // выводит их на консоль
+}
+
+module.exports = showTweets;
