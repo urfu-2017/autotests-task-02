c6126aa3808f5de8e679aa49b217cea5d5cc5fd8
Мишакова Наталья Сергеевна <mishakova.n@mail.ru>
2018-04-16 21:33:33 +0500

    Реализовала тесты

--- tests/index-test.js
+++ tests/index-test.js
@@ -6,15 +6,15 @@ const formatDate = require('../lib/formatDate');
 describe('showTweets', () => {
     it('should print date and tweet', () => {
 	const formatDate = sinon.stub();
-    formatDate.withArgs('2018-04-15T15:09:10.609Z').returns('вчера в 15:09');
-    formatDate.withArgs('2016-12-25T12:00:10.123Z').returns('25 марта 2017 года в 15:09');
+    formatDate.withArgs('2018-04-15T15:09:10.609Z').returns('РІС‡РµСЂР° РІ 15:09');
+    formatDate.withArgs('2016-12-25T12:00:10.123Z').returns('25 РјР°СЂС‚Р° 2017 РіРѕРґР° РІ 15:09');
     formatDate.throws('Illegal arguments');
 
     const showTweets = proxyquire('../lib/index', {
         './formatDate': formatDate
     });	
 	const actual = showTweets();
-	var result = "вчера в 15:09 Библиотека #nock позволяет не только удобно писать тесты, но и вести разработку фронтеда, в то время, когда бекенд ещё только проектируется! #urfu-testing-2017 25 марта 2017 года в 15:09 Для подмены модулей раньше я использовал #mockery, а сейчас всей душой полюбил #proxyquire. #urfu-testing-2017 Конец";
+	var result = "РІС‡РµСЂР° РІ 15:09 Р‘РёР±Р»РёРѕС‚РµРєР° #nock РїРѕР·РІРѕР»СЏРµС‚ РЅРµ С‚РѕР»СЊРєРѕ СѓРґРѕР±РЅРѕ РїРёСЃР°С‚СЊ С‚РµСЃС‚С‹, РЅРѕ Рё РІРµСЃС‚Рё СЂР°Р·СЂР°Р±РѕС‚РєСѓ С„СЂРѕРЅС‚РµРґР°, РІ С‚Рѕ РІСЂРµРјСЏ, РєРѕРіРґР° Р±РµРєРµРЅРґ РµС‰С‘ С‚РѕР»СЊРєРѕ РїСЂРѕРµРєС‚РёСЂСѓРµС‚СЃСЏ! #urfu-testing-2017 25 РјР°СЂС‚Р° 2017 РіРѕРґР° РІ 15:09 Р”Р»СЏ РїРѕРґРјРµРЅС‹ РјРѕРґСѓР»РµР№ СЂР°РЅСЊС€Рµ СЏ РёСЃРїРѕР»СЊР·РѕРІР°Р» #mockery, Р° СЃРµР№С‡Р°СЃ РІСЃРµР№ РґСѓС€РѕР№ РїРѕР»СЋР±РёР» #proxyquire. #urfu-testing-2017 РљРѕРЅРµС†";
 	assert.equal(actual, result);
 	});
 });
@@ -46,7 +46,7 @@ describe('formatDate', () => {
 	const actual = () => formatDate('2018-02-30T15:09:10.609Z'); assert.throws(actual, /non-existent date/);
 	 });
 	it('should print Only time for today', runSuccessTest('2018-04-16T15:09:10.609Z',"15:09"));
-	it('should print yesterday and time for yesterday', runSuccessTest('2018-04-15T15:09:10.609Z',"вчера в 15:09"));
-	it('should not print years for this year', runSuccessTest('2018-03-25T15:09:10.609Z', "25 марта в 15:09"));
-	it('should print all for previous year', runSuccessTest('2017-03-25T16:09:10.609Z', "25 марта 2017 года в 16:09"));
+	it('should print yesterday and time for yesterday', runSuccessTest('2018-04-15T15:09:10.609Z',"РІС‡РµСЂР° РІ 15:09"));
+	it('should not print years for this year', runSuccessTest('2018-03-25T15:09:10.609Z', "25 РјР°СЂС‚Р° РІ 15:09"));
+	it('should print all for previous year', runSuccessTest('2017-03-25T16:09:10.609Z', "25 РјР°СЂС‚Р° 2017 РіРѕРґР° РІ 16:09"));
 });
