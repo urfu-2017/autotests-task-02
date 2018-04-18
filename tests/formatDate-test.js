1b46cf66a8393aa4d4c7e1faa4ed35f67800dffc
Albina-G <erfolg1996@mail.ru>
2018-04-18 13:15:01 +0500

    Изменен формат теста

--- tests/formatDate-test.js
+++ tests/formatDate-test.js
@@ -1,20 +1,15 @@
 const proxyquire = require('proxyquire');
 const assert = require('assert');
 const sinon = require('sinon');
-const formatDate = require('../lib/formatDate');
+const moment = require('moment');
 
-function getTime(date) {
-    const time = new Date(date);
-    const min = time.getUTCMinutes() <= 9 ? '0' + time.getUTCMinutes() : time.getUTCMinutes();
-    const hour = time.getUTCHours() <= 9 ? '0' + time.getUTCHours() : time.getUTCHours();
-    return hour + ':' + min;
-}
+const formatDate = require('../lib/formatDate');
 
 const dataPositive = [
     { time: '2018-04-11T15:09:10.609Z', expected: '11 апреля в 15:09' },
     { time: '2016-12-25T12:00:10.123Z', expected: '25 декабря 2016 года в 12:00' },
-    { time: Date.now() - 3600 * 24 * 1000, expected: 'вчера в ' + getTime(Date.now()) },
-    { time: Date.now() - 3600 * 1000, expected: getTime(Date.now() - 3600 * 1000) },
+    { time: Date.now() - 3600 * 24 * 1000, expected: moment(Date.now() - 3600 * 24 * 1000).locale('ru').utc().format('вчера в HH:mm') },
+    { time: Date.now() - 3600 * 1000, expected: moment(Date.now() - 3600 * 1000).locale('ru').utc().format('HH:mm') },
     { time: '2018-02-11T15:09:10.609Z', expected: '11 февраля в 15:09' },
     { time: '2018-02-11T00:10:10.609Z', expected: '11 февраля в 00:10' }
 ];
--- tests/index-test.js
+++ tests/index-test.js
@@ -16,6 +16,7 @@ const tweets = [
 
 describe('showTweets', () => {
     it('should print tweets with new string', () => {
+        const print = sinon.spy();
         const formatDate = sinon.stub();
         formatDate.withArgs('2018-04-11T15:09:10.609Z').returns('11 апреля в 15:09');
         formatDate.withArgs('2016-12-25T12:00:10.123Z').returns('25 декабря 2016 года в 12:00');
@@ -26,13 +27,14 @@ describe('showTweets', () => {
         const showTweets = proxyquire(
             '../lib/index',
             { './getTweets': getTweets },            
-            { './formatDate': formatDate }
+            { './formatDate': formatDate },
+            { './print': print}
         );
         const expected = '11 апреля в 15:09\n' + 'Сообщение №1\n' + '25 декабря 2016 года в 12:00\n' +
             'Сообщение №2\n';
 
         const actual = showTweets();
-
+        
         assert.equal(actual, expected);
     });
 });
