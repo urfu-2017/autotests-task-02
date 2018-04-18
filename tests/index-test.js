35469a1f714c5a556e46985b1f3042cbc0e8e2e5
romassol <romass1998@mail.ru>
2018-04-18 20:13:46 +0500

    add test

--- tests/index-test.js
+++ tests/index-test.js
@@ -2,7 +2,21 @@ const proxyquire = require('proxyquire');
 const assert = require('assert');
 const sinon = require('sinon');
 const formatDate = require('../lib/formatDate')
-const getTweets = require('../lib/getTweets')
+
+describe('showTweets', () => {
+    // Этот тест написан для примера, его можно удалить
+    it('should print `Конец` after tweets', () => {
+        const print = sinon.spy();
+        const showTweets = proxyquire(
+            '../lib/index',
+            { './print': print }
+        );
+
+        showTweets();
+        assert.ok(print.callCount === 5);
+        assert.ok(print.calledWith('Конец'));
+    });
+});
 
 function getFromDateTest(inputDate, expectedDate) {
     return it(`should return '${expectedDate}' for [${inputDate}]`, () => {
