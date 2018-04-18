2d76210ca8f3546ca1c55593008747ecdf44a2d0
Ngbt95 <Mr.LinK.lin@gmail.com>
2018-04-18 15:08:05 +0500

    Реализовала formatDate, добавила тесты

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,19 +1,34 @@
 const proxyquire = require('proxyquire');
+// const showTweets = require('../lib/index');
 const assert = require('assert');
 const sinon = require('sinon');
 
-describe('showTweets', () => {
-    // Этот тест написан для примера, его можно удалить
-    it('should print `Конец` after tweets', () => {
+function runTest(string){
+    return () => {
+        const formatDate = sinon.stub();
         const print = sinon.spy();
-        const showTweets = proxyquire(
-            '../lib/index',
-            { './print': print }
-        );
+
+        formatDate.withArgs('2018-04-11T15:09:10.609Z').returns('15:09');
+        formatDate.withArgs('2016-12-25T12:00:10.123Z').returns('25 декабря 2016 года в 12:00');
+        const showTweets = proxyquire('../lib/index', {
+            '../lib/formatDate': formatDate,
+            './print': print
+        });
 
         showTweets();
 
-        assert.ok(print.calledOnce);
-        assert.ok(print.calledWith('Конец'));
+        assert.ok(print.calledWith(string));
+    }
+}
+
+describe('showTweets', () => {
+    it('should print first tweet', () => {
+        runTest('15:09' + '\n' + 'Библиотека #nock позволяет не только удобно писать тесты,' +
+            'но и вести разработку фронтеда, в то время, когда бекенд ещё только проектируется!' +
+            '#urfu-testing-2017' + '\n')
+    });
+    it('should print second tweet', () => {
+        runTest('25 декабря 2016 года в 12:00' + '\n' + 'Для подмены модулей раньше я использовал #mockery, ' +
+            'а сейчас всей душой полюбил #proxyquire. #urfu-testing-2017' + '\n')
     });
 });
