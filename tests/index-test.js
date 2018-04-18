13c50fc701120de1d0607ec1a6310f529a48b54c
Maxim <32168435+Rolyx2@users.noreply.github.com>
2018-04-18 17:04:10 +0500

    test

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,19 +1,30 @@
 const proxyquire = require('proxyquire');
 const assert = require('assert');
 const sinon = require('sinon');
+const moment = require('moment');
+const formatDate = require('../lib/formatDate');
 
-describe('showTweets', () => {
-    // Этот тест написан для примера, его можно удалить
-    it('should print `Конец` after tweets', () => {
-        const print = sinon.spy();
-        const showTweets = proxyquire(
-            '../lib/index',
-            { './print': print }
-        );
-
-        showTweets();
-
-        assert.ok(print.calledOnce);
-        assert.ok(print.calledWith('Конец'));
+describe('Проверка даты', () => {
+    it('should return `Not valid date` for Дата', () => {
+        try{
+            formatDate('Дата');
+            throw new Error('`formatDate` should throw error')
+        } catch (error) {
+            assert.equal(error.message, 'Not valid date');
+        }
     });
+    [
+        { date: moment().startOf('day').utc().format(), expected: '00:00' },
+        { date: moment().startOf('day').add(1, 'minute').utc().format(), expected: '00:01' },
+        { date: moment().startOf('day').subtract(1, 'day').utc().format(), expected: 'вчера в 00:00' },
+        { date: moment().subtract(1, 'day').utc().format(), expected: `вчера в ${moment().format('HH:mm')}` },
+        { date: moment().subtract(1, 'month').format(), expected: `${moment().subtract(1, 'month').locale('ru').format('DD MMMM [в] HH:mm')}`},
+        { date: moment().subtract(1, 'year').format(), expected: `${moment().subtract(1, 'year').locale('ru').format('DD MMMM YYYY [года в] HH:mm')}`},
+        { date:'2016-10-05T12:00:10.123Z', expected: '05 октября 2016 года в 17:00'},
+        ].forEach(({ date, expected }) =>
+            it(`should return ${expected} for ${date}`, () => {
+                const actual = formatDate(date);
+                assert.equal(actual, expected);
+            })
+        );
 });
