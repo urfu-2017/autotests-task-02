e5ffde9431df975159886551b2d6cadc8d21bb18
Poshekhonova Olya <o.poshekhonova@gmail.com>
2018-04-18 17:07:14 +0500

    '.'

--- tests/index-test.js
+++ tests/index-test.js
@@ -8,12 +8,38 @@ describe('showTweets', () => {
         const print = sinon.spy();
         const showTweets = proxyquire(
             '../lib/index',
-            { './print': print }
+            { '../lib/print': print }
         );
 
         showTweets();
 
         assert.ok(print.calledOnce);
         assert.ok(print.calledWith('Конец'));
+    });
+});
+describe('formatDate', () => {
+    it('should return 15:09', () => {
+           const formatDate = sinon.spy({formatDate:require('../lib/formatDate')},'formatDate');
+                var test1 = new Date('2018-04-18T15:09:10.609Z')
+
+                assert.equal('15:09', formatDate(test1));
+        });
+    it('should return 11 Апреля 2015 года в 15:09', () => {
+        const formatDate = sinon.spy({formatDate:require('../lib/formatDate')},'formatDate');
+        var test1 = new Date('2015-04-11T15:09:10.609Z')
+
+        assert.equal('11 Апреля 2015 года в 15:09', formatDate(test1));
+    });
+    it('should return вчера в 15:09', () => {
+        const formatDate = sinon.spy({formatDate:require('../lib/formatDate')},'formatDate');
+        var test1 = new Date('2018-04-17T15:09:10.609Z')
+
+        assert.equal('вчера в 15:09', formatDate(test1));
+    });
+    it('should return 17 Марта в 15:09', () => {
+        const formatDate = sinon.spy({formatDate:require('../lib/formatDate')},'formatDate');
+        var test1 = new Date('2018-03-17T15:09:10.609Z')
+
+        assert.equal('17 Марта в 15:09', formatDate(test1));
     });
 });
