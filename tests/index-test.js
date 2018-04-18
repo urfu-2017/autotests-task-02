fa8375d1837a5d620245eb7ac88ec68ba717b9d2
Anastasija Vasilenko <vasilenko.nastya@mail.ru>
2018-04-18 18:33:31 +0500

    Изменения

--- tests/index-test.js
+++ tests/index-test.js
@@ -64,8 +64,8 @@ function runSuccessTest(date, expected){
             const actual= () => formatDate('2018-04-30T15:09:10.609Z');
             assert.throws(actual, /Future day/);
         });
-        it('should throw new Error \'Future time\' for date \'2018-04-14T23:09:10.609Z\'', () => {
-            const actual= () => formatDate('2018-05-20T15:09:10.609Z');
+        it('should throw new Error \'Future time\' for date \'2018-04-18T23:09:10.609Z\'', () => {
+            const actual= () => formatDate('2018-04-18T23:09:10.609Z');
             assert.throws(actual, /Future time/);
         });
 
@@ -81,15 +81,15 @@ function runSuccessTest(date, expected){
 describe('showTweets', () => {
     it('should print date and tweet', () => {
         const formatDate = sinon.stub();
-        formatDate.withArgs('2018-04-15T15:09:10.609Z').returns('вчера в 15:09');
-        formatDate.withArgs('2016-12-25T12:00:10.123Z').returns('25 марта 2016 года в 15:09');
+        formatDate.withArgs('2018-04-17T15:09:10.609Z').returns('вчера в 15:09');
+        formatDate.withArgs('2016-12-25T12:00:10.123Z').returns('25.03.2016 в 15:09');
         formatDate.throws('Illegal arguments');
 
         const showTweets = proxyquire('../lib/index', {
             './formatDate': formatDate
         });
         const actual = showTweets();
-        var result = "вчера в 15:09 Библиотека #nock позволяет не только удобно писать тесты, но и вести разработку фронтеда, в то время, когда бекенд ещё только проектируется! #urfu-testing-2017 25 марта 2016 года в 15:09 Для подмены модулей раньше я использовал #mockery, а сейчас всей душой полюбил #proxyquire. #urfu-testing-2017 Конец";
+        var result = "вчера в 15:09 Библиотека #nock позволяет не только удобно писать тесты, но и вести разработку фронтеда, в то время, когда бекенд ещё только проектируется! #urfu-testing-2017 25.03.2016 в 15:09 Для подмены модулей раньше я использовал #mockery, а сейчас всей душой полюбил #proxyquire. #urfu-testing-2017 Конец";
         assert.equal(actual, result);
     });
 });
