982a52610a5673b9432f1abbb5934a408337f28a
Anastasija Vasilenko <vasilenko.nastya@mail.ru>
2018-04-18 18:24:12 +0500

    Тестирование

--- tests/index-test.js
+++ tests/index-test.js
@@ -65,43 +65,31 @@ function runSuccessTest(date, expected){
             assert.throws(actual, /Future day/);
         });
         it('should throw new Error \'Future time\' for date \'2018-04-14T23:09:10.609Z\'', () => {
-            const actual= () => formatDate('2018-04-14T15:09:10.609Z');
+            const actual= () => formatDate('2018-05-14T15:09:10.609Z');
             assert.throws(actual, /Future time/);
         });
 
         it('should print Only time for today', runSuccessTest('2018-04-18T10:09:10.609Z',"10:09"));
         it('should print yesterday and time for yesterday', runSuccessTest('2018-04-17T15:09:10.609Z',"вчера в 15:09"));
         it('should not print years for this year', runSuccessTest('2018-02-15T15:09:10.609Z', "15 февраля в 15:09"));
-        it('should print all for previous year', runSuccessTest('2017-03-25T16:09:10.609Z', "25.03.2017 года в 16:09"));
+        it('should print all for previous year', runSuccessTest('2017-03-25T16:09:10.609Z', "25.03.2017 в 16:09"));
 
     });
 
 
 
 describe('showTweets', () => {
-    it('should print tweets with new string', () => {
-        const print = sinon.spy();
+    it('should print date and tweet', () => {
         const formatDate = sinon.stub();
-        formatDate.withArgs('2018-04-17T15:09:10.609Z').returns('17 апреля в 15:09');
-        formatDate.withArgs('2010-05-02T09:30:10.123Z').returns('2 мая 2010 года в 09:30');
-        formatDate.threw('Illegal arguments');
-        const getTweets = sinon.stub();
-        getTweets.withArgs('#urfu-testing-2017').returns(tweets);
-        getTweets.threw('Illegal arguments');
-        const showTweets = proxyquire(
-            '../lib/index',
-            { './getTweets': getTweets },
-            { './formatDate': formatDate },
-            { './print': print}
-        );
-        const expected = '17 апреля в 15:09\n' + 'Сообщение №1\n' + '2 мая 2010 года в 09:30\n' +
-            'Сообщение №2\n';
+        formatDate.withArgs('2018-04-15T15:09:10.609Z').returns('вчера в 15:09');
+        formatDate.withArgs('2016-12-25T12:00:10.123Z').returns('25 марта 2016 года в 15:09');
+        formatDate.throws('Illegal arguments');
 
+        const showTweets = proxyquire('../lib/index', {
+            './formatDate': formatDate
+        });
         const actual = showTweets();
-
-        assert.equal(actual, expected);
+        var result = "вчера в 15:09 Библиотека #nock позволяет не только удобно писать тесты, но и вести разработку фронтеда, в то время, когда бекенд ещё только проектируется! #urfu-testing-2017 25 марта 2016 года в 15:09 Для подмены модулей раньше я использовал #mockery, а сейчас всей душой полюбил #proxyquire. #urfu-testing-2017 Конец";
+        assert.equal(actual, result);
     });
 });
-
-
-
