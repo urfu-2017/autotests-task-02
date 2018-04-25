cb4a11c50981d744e73994a58dd1f90ef2ea7b60
NeosKsen <raketka321@mail.ru>
2018-04-25 12:20:34 +0500

    testfix

--- tests/index-test.js
+++ tests/index-test.js
@@ -2,29 +2,42 @@ const proxyquire = require('proxyquire');
 const assert = require('assert');
 const sinon = require('sinon');
 const formatDate = require('../lib/formatDate');
-const moment = require('moment');
 
-describe("formatDate", () => {
-    [
-        { date: "", error: "Date is not valid" },
-        { date: "1994-13-31T00:00:00.000Z", error: "Date is not valid" },
-        { date: "9999-12-31T00:00:00.000Z", error: "Tweet from future"}
-    ].forEach(({ date, error }) =>
-            it(`should throw "${error}" for "${date}"`, () => {
-            assert.throws(() => formatDate(new Date(date)), error);
-        }));
+const NOW = new Date('2018-04-18T01:01:10.609Z');
 
-    [
-        { date: moment().utc().startOf("day").format(), expectedResult: "00:00" },
-        { date: moment().utc().startOf("day").add(5, "minute").format(), expectedResult: "00:05" },
-        { date: moment().utc().startOf("day").subtract(1, "day").format(), expectedResult: "вчера в 00:00" },
-        { date: moment().utc().startOf("year").add(2, "month").format(), expectedResult: "1 марта в 00:00" },
-        { date: moment().utc().startOf("year").subtract(1, "day").format(), expectedResult: "31 декабря 2017 года в 00:00" },
-        { date: "2016-12-25T12:00:10.123Z", expectedResult: "25 декабря 2016 года в 12:00" },
-    ].forEach(({ date, expectedResult }) =>
-            it(`should return ${expectedResult} from ${date}`, () => {
-            const actual = formatDate(new Date(date));
-            assert.equal(actual, expectedResult);
-        })
-    );
+describe('formatDate', () => {
+    let clock = null;
+    before(() => {
+        clock = sinon.useFakeTimers(NOW);
+    });
+    describe('Positive tests', () => {
+        [
+            { date: '2018-04-18T00:01:10.609Z', expected: '00:01' },
+            { date: '2018-04-17T00:01:10.609Z', expected: 'вчера в 00:01' },
+            { date: '2018-03-25T00:01:10.609Z', expected: '25 марта в 00:01' },
+            { date: '2017-03-25T00:01:10.609Z', expected: '25 марта 2017 года в 00:01' },
+            { date: '2017-01-01T00:01:10.609Z', expected: '1 января 2017 года в 00:01' },
+            { date: '2016-12-31T00:01:10.609Z', expected: '31 декабря 2016 года в 00:01' }
+        ].forEach(({ date, expected }) =>
+            it(`should return ${expected} for ${date}`, () => {
+                const actual = formatDate(date);
+                assert.equal(actual, expected);
+            })
+        );
+    });
+
+    describe('Negative tests', () => {
+        [
+            { date: "", error: "Date is not valid" },
+            { date: "1994-13-31T00:00:00.000Z", error: "Date is not valid" },
+            { date: "9999-12-31T00:00:00.000Z", error: "Tweet from future"}
+        ].forEach(({ date, error }) =>
+                it(`should throw "${error}" for "${date}"`, () => {
+                assert.throws(() => formatDate(new Date(date)), error);
+            }));
+    });
+
+    after(() => {
+        clock.restore();
+    });
 });
