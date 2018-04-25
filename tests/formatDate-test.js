2376a1907ad329f92e187838fd5c64507b287fce
Maxim <32168435+Rolyx2@users.noreply.github.com>
2018-04-25 14:14:26 +0500

    Add files via upload

---
+++ tests/formatDate-test.js
@@ -0,0 +1,31 @@
+const proxyquire = require('proxyquire');
+const assert = require('assert');
+const sinon = require('sinon');
+const moment = require('moment');
+const formatDate = require('../lib/formatDate');
+const today = moment();
+
+describe('Проверка даты', () => {
+    [
+        { date: '', expected: 'Not valid date' },
+        { date: 'Дата', expected: 'Not valid date' },].forEach(({ date, expected }) =>
+    it(`should throw error '${expected}' for '${date}'`, ()=> {
+        assert.throws(() => formatDate(date), expected);
+    }));
+
+
+    [
+        { date: moment(today).startOf('day').utc().format(), expected: '00:00' },
+        { date: moment(today).startOf('day').add(1, 'minute').utc().format(), expected: '00:01' },
+        { date: moment(today).startOf('day').subtract(1, 'day').utc().format(), expected: 'вчера в 00:00' },
+        { date: moment(today).subtract(1, 'day').utc().format(), expected: `вчера в ${moment(today).format('HH:mm')}` },
+        { date: moment(today).subtract(1, 'month').format(), expected: `${moment(today).subtract(1, 'month').locale('ru').format('DD MMMM [в] HH:mm')}`},
+        { date: moment(today).subtract(1, 'year').format(), expected: `${moment(today).subtract(1, 'year').locale('ru').format('DD MMMM YYYY [года в] HH:mm')}`},
+        { date:'2016-12-25T12:00:10.123Z', expected: '25 декабря 2016 года в 17:00'},
+        ].forEach(({ date, expected }) =>
+            it(`should return ${expected} for ${date}`, () => {
+                const actual = formatDate(date);
+                assert.equal(actual, expected);
+            })
+        );
+});
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,29 +1,18 @@
 const proxyquire = require('proxyquire');
 const assert = require('assert');
 const sinon = require('sinon');
-const moment = require('moment');
-const formatDate = require('../lib/formatDate');
-
-describe('Проверка даты', () => {
-    it('should return `Not valid date` for Дата', () => {
-        try{
-            formatDate('Дата');
-            throw new Error('`formatDate` should throw error')
-        } catch (error) {
-            assert.equal(error.message, 'Not valid date');
-        }
-    });
-    [
-        { date: moment().startOf('day').utc().format(), expected: '00:00' },
-        { date: moment().startOf('day').add(1, 'minute').utc().format(), expected: '00:01' },
-        { date: moment().startOf('day').subtract(1, 'day').utc().format(), expected: 'вчера в 00:00' },
-        { date: moment().subtract(1, 'day').utc().format(), expected: `вчера в ${moment().format('HH:mm')}` },
-        { date: moment().subtract(1, 'month').format(), expected: `${moment().subtract(1, 'month').locale('ru').format('DD MMMM [в] HH:mm')}`},
-        { date: moment().subtract(1, 'year').format(), expected: `${moment().subtract(1, 'year').locale('ru').format('DD MMMM YYYY [года в] HH:mm')}`},
-        ].forEach(({ date, expected }) =>
-            it(`should return ${expected} for ${date}`, () => {
-                const actual = formatDate(date);
-                assert.equal(actual, expected);
-            })
+ 
+describe('showTweets', () => {
+    it('should print call counts 4 ', () => {
+        const print = sinon.spy();
+        const showTweets = proxyquire(
+            '../lib/index',
+            { './print': print }
         );
+
+        showTweets();
+
+        assert.equal(print.callCount, 4);
+    });
 });
+
