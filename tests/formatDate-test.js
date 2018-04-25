529d65074780dd1d039b21ee4f985000b5f6ec60
Dima Stark <dvstark@yandex.ru>
2018-04-25 21:11:53 +0500

    Разделил тесты на разные файлы + мелкие правки по ревью

---
+++ tests/formatDate-test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const assert = require('assert');
+const moment = require('moment');
+const tk = require('timekeeper');
+
+const formatDate = require('../lib/formatDate');
+
+describe('formatDate', () => {
+    const now = new Date('2018.04.12 12:42:08.232').getTime(); // 2018.04.12 12:42:08.232
+    const testCases = [
+        { arg: now, expected: '12:42' },
+        { arg: moment(now).startOf('day').toDate(), expected: '00:00' },
+        { arg: moment(now).subtract(1, 'days').toDate(), expected: 'Вчера в 12:42' },
+        { arg: moment(now).subtract(1, 'days').startOf('day').toDate(), expected: 'Вчера в 00:00' },
+        { arg: moment(now).subtract(2, 'days').toDate(), expected: '10 апреля в 12:42' },
+        { arg: moment(now).subtract(1, 'months').toDate(), expected: '12 марта в 12:42' },
+        { arg: moment(now).subtract(1, 'years').toDate(), expected: '12 апреля 2017 года в 12:42' },
+        { arg: moment(now).startOf('year').toDate(), expected: '01 января в 00:00' },
+        { arg: {}, expected: 'Invalid date' },
+        { arg: [], expected: 'Invalid date' },
+        { arg: null, expected: 'Invalid date' },
+        { arg: undefined, expected: 'Invalid date' },
+    ];
+
+    before(() => tk.freeze(now));
+
+    testCases.forEach(testCase => {
+        it(`should return '${testCase.expected}' for ${JSON.stringify(testCase.arg)}`, () => {
+            const actual = formatDate(testCase.arg);
+
+            assert.equal(actual, testCase.expected);
+        });
+    });
+
+    after(() => tk.reset());
+});
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,25 +1,10 @@
 'use strict';
 
 const assert = require('assert');
-const moment = require('moment');
 const proxyquire = require('proxyquire');
 const sinon = require('sinon');
-const tk = require('timekeeper');
-
-const formatDate = require('../lib/formatDate');
 
 describe('showTweets', () => {
-    it('calls print only once', () => {
-        const print = sinon.spy();
-        const lib = proxyquire('../lib/index', {
-            './print': print
-        });
-
-        lib.showTweets();
-
-        assert.ok(print.calledOnce);
-    });
-
     it('should return empty string if there no tweets', () => {
         const print = sinon.spy();
         const lib = proxyquire('../lib/index', {
@@ -29,6 +14,7 @@ describe('showTweets', () => {
 
         lib.showTweets();
 
+        assert.ok(print.calledOnce);
         sinon.assert.calledWith(print, sinon.match(''));
     });
 
@@ -39,51 +25,25 @@ describe('showTweets', () => {
             './formatDate': date => date,
             './getTweets': () => [
                 {
-                    created_at: 1,
-                    text: '1'
+                    created_at: 'date-1',
+                    text: 'text-1'
                 },
                 {
-                    created_at: 2,
-                    text: '2'
+                    created_at: 'date-2',
+                    text: 'text-2'
                 },
                 {
-                    created_at: 3,
-                    text: '3'
+                    created_at: 'date-3',
+                    text: 'text-3'
                 }
             ]
         });
 
         lib.showTweets();
 
-        sinon.assert.calledWith(print, sinon.match('1\n1\n\n2\n2\n\n3\n3'));
+        assert.ok(print.calledOnce);
+        sinon.assert.calledWith(print, sinon.match(
+            'date-1\ntext-1\n\ndate-2\ntext-2\n\ndate-3\ntext-3'
+        ));
     });
-});
-
-describe('formatDate', () => {
-    const now = 1523536928232; // 2018.04.12 12:42:08.232
-    const testCases = [
-        { arg: now, expected: '12:42' },
-        { arg: moment(now).startOf('day').toDate(), expected: '00:00' },
-        { arg: moment(now).subtract(1, 'days').toDate(), expected: 'Вчера в 12:42' },
-        { arg: moment(now).subtract(1, 'days').startOf('day').toDate(), expected: 'Вчера в 00:00' },
-        { arg: moment(now).subtract(2, 'days').toDate(), expected: '10 апреля в 12:42' },
-        { arg: moment(now).subtract(1, 'months').toDate(), expected: '12 марта в 12:42' },
-        { arg: moment(now).subtract(2, 'months').toDate(), expected: '12 февраля в 12:42' },
-        { arg: moment(now).subtract(1, 'years').toDate(), expected: '12 апреля 2017 года в 12:42' },
-        { arg: moment(now).startOf('year').toDate(), expected: '01 января в 00:00' },
-        { arg: {}, expected: 'Invalid date' },
-        { arg: [], expected: 'Invalid date' },
-        { arg: null, expected: 'Invalid date' },
-        { arg: undefined, expected: 'Invalid date' },
-    ];
-
-    before(() => tk.freeze(now));
-
-    testCases.forEach(testCase => {
-        it(`should return '${testCase.expected}' for ${JSON.stringify(testCase.arg)}`, () => {
-            assert.equal(formatDate(testCase.arg), testCase.expected);
-        });
-    });
-
-    after(() => tk.reset());
 });
