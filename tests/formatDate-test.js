7a5779e10100cdfffda62287f4713e09ffec6be2
dimastark <dimastark@yandex-team.ru>
2018-05-07 13:40:09 +0500

    Переделал возвращение 'Invalid date' на бросание ошибки

--- tests/formatDate-test.js
+++ tests/formatDate-test.js
@@ -8,7 +8,10 @@ const formatDate = require('../lib/formatDate');
 
 describe('formatDate', () => {
     const now = new Date('2018.04.12 12:42:08.232').getTime();
-    const testCases = [
+
+    before(() => tk.freeze(now));
+
+    [
         { arg: now, expected: '12:42' },
         { arg: moment(now).startOf('day').toDate(), expected: '00:00' },
         { arg: moment(now).subtract(1, 'days').toDate(), expected: 'Вчера в 12:42' },
@@ -16,22 +19,25 @@ describe('formatDate', () => {
         { arg: moment(now).subtract(2, 'days').toDate(), expected: '10 апреля в 12:42' },
         { arg: moment(now).subtract(1, 'months').toDate(), expected: '12 марта в 12:42' },
         { arg: moment(now).subtract(1, 'years').toDate(), expected: '12 апреля 2017 года в 12:42' },
-        { arg: moment(now).startOf('year').toDate(), expected: '01 января в 00:00' },
-        { arg: {}, expected: 'Invalid date' },
-        { arg: [], expected: 'Invalid date' },
-        { arg: null, expected: 'Invalid date' },
-        { arg: undefined, expected: 'Invalid date' },
-    ];
-
-    before(() => tk.freeze(now));
-
-    testCases.forEach(testCase => {
+        { arg: moment(now).startOf('year').toDate(), expected: '01 января в 00:00' }
+    ].forEach(testCase =>
         it(`should return '${testCase.expected}' for ${JSON.stringify(testCase.arg)}`, () => {
             const actual = formatDate(testCase.arg);
 
             assert.equal(actual, testCase.expected);
-        });
-    });
+        })
+    );
+
+    [
+        { arg: {}, expectedError: /Invalid date/ },
+        { arg: [], expectedError: /Invalid date/ },
+        { arg: null, expectedError: /Invalid date/ },
+        { arg: undefined, expectedError: /Invalid date/ }
+    ].forEach(testCase =>
+        it(`should raise '${testCase.expectedError}' for ${JSON.stringify(testCase.arg)}`, () => {
+            assert.throws(() => formatDate(testCase.arg), testCase.expectedError);
+        })
+    );
 
     after(() => tk.reset());
 });
