10b7dc8a00aa345c6b622b8742a59b9640594603
Andrey <crashc@yandex.ru>
2018-05-14 21:28:14 +0500

    Update index-test.js
    Rewrite full file.

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,16 +1,16 @@
 const proxyquire = require('proxyquire');
 const assert = require('assert');
 const sinon = require('sinon');
-const showTweets = require('../lib/index');
+const formatDate = require('../lib/formatDate');
 
 describe('showTweets', () => {
-    const formatDate = sinon.stub();
-    // Этот тест написан для примера, его можно удалить
     it('should print `Конец` after tweets', () => {
         const print = sinon.spy();
         const showTweets = proxyquire(
             '../lib/index',
-            { './print': print }
+            {
+                './print': print
+            }
         );
 
         showTweets();
@@ -18,90 +18,55 @@ describe('showTweets', () => {
         assert.ok(print.calledWith('Конец'));
     });
 
-    it('should print tweets', () => {
-        let date1 = '2018-04-17T22:15:31.609Z';
-        let date2 = '2018-04-16T22:15:31.609Z';
-       
-        formatDate.withArgs(date1).returns('22:15');
-        formatDate.withArgs(date2).returns('вчера 22:15');
-
-        const getTweets = sinon.stub();
-
-        getTweets.returns([
-            {
-                created_at: date1,
-                text: 'Тестовые данные'
-            },{
-                created_at: date2,
-                text: 'Тестовые данные 2'
-            }
-        ]);
-
+    it('should print `вчера в 15:09` after tweets', () => {
+        const formatDate = sinon.spy();
         const print = sinon.spy();
-        const showTweets = proxyquire('../lib/index', {
-            './getTweets' : getTweets,
-            './formatDate': formatDate,
-            './print' : print
-        });
+        const showTweets = proxyquire(
+            '../lib/index',
+            {
+                './formatDate': formatDate,
+                './print': print
+            }
+        );
+
         showTweets();
 
-        assert.ok(print.calledWith('22:15\nТестовые данные\n'));
-        assert.ok(print.calledWith('вчера 22:15\nТестовые данные 2\n'));
-        assert.ok(print.calledWith('Конец'));
+        assert.ok(formatDate.calledWith('2018-04-11T15:09:10.609Z'));
     });
 
-    it ('should print tweet with date', () => {
-        formatDate.withArgs('2018-04-17T11:33:15.609Z').returns('Вчера в 11:33');
-        formatDate.withArgs('2017-03-18T11:33:10.609Z').returns('18 марта 2017 года в 11:33');
-        formatDate.throws('Illegal arguments');
-            const showTweets = proxyquire('../lib/index', {
-            './formatDate': formatDate
-            });
-    });
-    it ('should throw error when tweet date is in future', () => {
-        const print = sinon.spy();
-        const showTweets = proxyquire('../lib/index', {
-            './print': print,
-            "./getTweets": () => [
-                { created_at: "3050-03-25T16:09:10.609Z",
-                text: "text"
-                }]
-            });
+    it('should return `incorrect data` for "kekeke"', () => {
+        const actual = formatDate('kekeke');
+
+        assert.equal(actual, 'incorrect data');
     });
 
-    describe('Negative tests for dateFormat', () => {
-        const negativeCases = [
-            {date: null,        expectedMessage: 'date must be a String'},
-            {date: undefined,   expectedMessage: 'date must be a String'},
-            {date: 1,           expectedMessage: 'date must be a String'},
-            {date: 'Date',      expectedMessage: 'date must be a String'},
-            {date: 3.3,         expectedMessage: 'date must be a String'},
-            {date: NaN,         expectedMessage: 'date must be a String'},
-            {date: {},          expectedMessage: 'date must be a String'},
-            {date: new Date(),  expectedMessage: 'incorrect format'},
-            {date: '2018-04-18L12:00:00.000Z',  expectedMessage: 'incorrect format'},
-            {date: '2018-04-55T12:00:00.000Z',  expectedMessage: 'incorrect format'},
-        ]
-            negativeCases.forEach(function(Cases){
-            it (`should trow error "${Cases.expectedMessage}" for "${Cases.date}"`, () => {
-                const actual = () => formatDate(Cases.date);
-                assert.throws(actual, Cases.expectedMessage);
-            })
-        })
+    it('should return `incorrect data` for ""', () => {
+        const actual = formatDate('');
+
+        assert.equal(actual, 'incorrect data');
     });
 
-    describe ('Positive tests for dateFormat', () => {
-        const positiveCases = [
-            {data: '2018-04-18T16:43:11.609Z', expectedMessage: '18 апреля в 16:43'},
-            {data: '2018-04-18T00:00:00.000Z', expectedMessage: '18 апреля в 00:00'},
-            {data: '2017-04-18T00:00:00.000Z', expectedMessage: '18 апреля 2017 года в 00:00'},
-        ]
-    
-        positiveCases.forEach(function(Cases){
-        it (`should trow error "${Cases.expectedMessage}" for "${Cases.data}" `, () => {
-            const actual = () => formatDate(Cases.data);
-            assert.throws(actual, Cases.expectedMessage);
-        })
-        })
+    it('should return `incorrect data` for NaN', () => {
+        const actual = formatDate(NaN);
+
+        assert.equal(actual, 'incorrect data');
+    });
+
+    it('should return `incorrect data` for undefined', () => {
+        const actual = formatDate(undefined);
+
+        assert.equal(actual, 'incorrect data');
+    });
+
+    it('should return `25 марта в 15:09` for "2018-03-25T15:09:10.609Z"', () => {
+        const actual = formatDate("2018-03-25T15:09:10.609Z");
+
+        assert.equal(actual, '25 марта в 15:09');
+    });
+
+    it('should return `25 марта 2017 года в 16:09` for "2017-03-25T16:09:10.609Z"', () => {
+        const actual = formatDate("2017-03-25T16:09:10.609Z");
+
+        assert.equal(actual, '25 марта 2017 года в 16:09');
     });
 });
